44eca9d6ee02b8240390f9a9ee122b5997ed0801
Timothy Tampold <timothytampold@gmail.com>
2023-01-17 15:18:12 +0100

    Landing page UI done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,25 @@ import type { NextPage } from 'next';
 
 const Index: NextPage = () => {
   return (
-    <div>
+    <main className="mx-auto flex min-h-screen w-[min(75%,20rem)] flex-col items-center justify-center">
       <InstagramTextLogo />
-    </div>
+
+      <button className="mt-8 w-full rounded-lg bg-brand-secondary p-2 font-medium text-accent-primary hover:bg-brand-secondary/90">
+        Log in with Google
+      </button>
+
+      <div className="my-4 grid w-full grid-cols-[1fr_auto_1fr] items-center gap-4">
+        <div className="h-[0.0625rem] bg-accent-tertiary/50"></div>
+        <span className="text-sm font-medium uppercase text-accent-tertiary">
+          Or
+        </span>
+        <div className="h-[0.0625rem] bg-accent-tertiary/50"></div>
+      </div>
+
+      <button className="w-full rounded-lg bg-brand-secondary p-2 font-medium text-accent-primary hover:bg-brand-secondary/90">
+        Log in with demo account
+      </button>
+    </main>
   );
 };
 
